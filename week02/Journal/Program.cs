4df404e37ea5570cde581bd40b5a9a17b2979c0b
Bruno Souza <brunosouza141@gmail.com>
2025-07-11 03:28:22 -0300

    Journal

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -6,7 +6,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("123");
+        int userChoice = 0;
+
+        //Instanciate the classes
+        Entry entry = new Entry();
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        //First interaction with the user
+        Console.WriteLine("Welcome to the Journal program");
+        //Do while the user choose the option 5 (Quit)
+        do
+        {
+            Console.WriteLine("Please select one of the following choices.");
+            Console.WriteLine("1. Write\n2. Display\n3. Load\n4. Save\n5. Quit");
+            Console.Write("What would you like to do: ");
+            userChoice = int.Parse(Console.ReadLine());
+
+            switch (userChoice)
+            {
+                case 1:
+                    // Get date as string
+                    DateTime theCurrentTime = DateTime.Now;
+                    string dateText = theCurrentTime.ToShortDateString();
+
+                    // Get random prompt and show the user
+                    string randomPrompt = promptGenerator.GetRandomPrompt();
+                    Console.WriteLine(randomPrompt);
+
+                    // Add entry details to the Entry instance.
+                    entry._entryText = Console.ReadLine();
+                    entry._promptText = randomPrompt;
+                    entry._date = dateText;
+
+                    journal.AddEntry(entry);
+
+
+                    break;
+
+                case 2:
+
+
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+            }
+        } while (userChoice != 5);
 
     }
     
