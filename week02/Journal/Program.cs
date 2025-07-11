64d14d1997dbe792a2d3538b8f9393804cff8973
Bruno Souza <brunosouza141@gmail.com>
2025-07-11 01:47:27 -0300

    Journal

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,10 @@
 using System;
-
+using System.IO;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        
+
     }
 }
