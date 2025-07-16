8118a1e8876eaf556ecd43973a14dddc042029af
Bruno Souza <brunosouza141@gmail.com>
2025-07-16 00:02:40 -0300

    Create Fractions function

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction = new Fraction();
+        Fraction fraction1 = new Fraction(5);
+        Fraction fraction2 = new Fraction(3, 4);
+        Fraction fraction3 = new Fraction(1, 3);
+        Console.WriteLine(fraction.GetTop() + "/" + fraction.GetBottom());
+        Console.WriteLine(fraction.GetDecimalValue());
+        Console.WriteLine(fraction1.GetTop() + "/" + fraction1.GetBottom());
+        Console.WriteLine(fraction1.GetDecimalValue());
+        Console.WriteLine(fraction2.GetTop() + "/" + fraction2.GetBottom());
+        Console.WriteLine(fraction2.GetDecimalValue());
+        Console.WriteLine(fraction3.GetTop() + "/" + fraction3.GetBottom());
+        Console.WriteLine(fraction3.GetDecimalValue());
     }
 }
